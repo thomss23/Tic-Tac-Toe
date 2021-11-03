a006a9e04920bc1d3b2824a46375d16dd8014610
Thom VM <borbelithomas@gmail.com>
2021-11-03 17:07:42 +0200

    finish UI

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/*
+====PLAN====
+1) Setup a main menu where the user could select whether to play vs another player or vs AI - done
+2) After the user selects an option, setup HTML && CSS for the board - done
+3) Write PVP logic first
+4) Figure out PVE logic using minimax algorithm
+*/
+
